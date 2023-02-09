159e0a3fa36c9aaef3c6a5ce574ef237a09e7321
abbeyrawr <113481669+abbeyrawr@users.noreply.github.com>
2023-02-08 21:05:39 -0600

    Update 2_person_dict.py

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -1,3 +1,4 @@
+# finished
 person = {}
 person["fname"] = "Joe"
 person["lname"] = "Fonebone"
@@ -12,11 +13,13 @@ person["pets"] = {"dog": "Fido", "cat": "Sox"}
 print(person["children"][1])
 
 # print out the name of the cat
-print(person["pets"])
-
+print(person["pets"]["cat"])
 
 # iterate through all children and print out each child
-
+for y in person["children"]:
+    print(y)
 
 # Print out the pets in this format
 # type of pet: dog name of pet: Fido
+for key, value in person["pets"].items():
+    print(f"type of pet: {key} name of pet: {value}")
