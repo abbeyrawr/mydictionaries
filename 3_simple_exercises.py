14ea202c6dbec75adfac8737534c8c875d29f6b2
abbeyrawr <113481669+abbeyrawr@users.noreply.github.com>
2023-02-08 21:05:10 -0600

    Update 3_simple_exercises.py

--- 3_simple_exercises.py
+++ 3_simple_exercises.py
@@ -1,4 +1,5 @@
 # 1) print out the value for the key 'history' using the dictionary below
+#finished
 
 
 sampleDict = { 
@@ -13,14 +14,16 @@ sampleDict = {
    }
 }
 
-
+x = sampleDict["class"]["student"]["marks"]["history"]
+print (x)
 
 
 # 2) Add 2 inches to the son's height.
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
-
+dict["son's height"] += 2
+print(dict)
 
 
 
@@ -32,7 +35,8 @@ sampleDict = {
      'emp3': {'name': 'Brad', 'salary': 6500}
 }
 
-
+sampleDict["emp3"]["salary"] = 8500
+print(sampleDict)
 
 
 
@@ -43,3 +47,5 @@ sampleDict = {
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
 
+dict["work"] = ["Apology", "Phaedo", "Republic", "Symposium"]
+print (dict)
